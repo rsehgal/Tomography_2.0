b9c0f7287b126d2fb2b26ffbd2e1d68dfab4c786
Raman Sehgal <sc.ramansehgal@gmail.com>
2022-04-04 10:29:15 +0530

    Added time profiling for processing of PoCA points in scalar and vector mode. Performance gain of the order of 2.5 for double and 5.2 for single precision are obtained.

---
+++ include/timer.h
@@ -0,0 +1,65 @@
+#ifndef VECCORE_TIMER_H
+#define VECCORE_TIMER_H
+
+#include <chrono>
+
+using namespace std::chrono;
+
+template <class unit = nanoseconds>
+class Timer {
+  using clock = high_resolution_clock;
+
+public:
+  Timer() : fStart(), fStop() { Start(); }
+
+  void Reset() { Start(); }
+
+  void Start() { fStart = clock::now(); }
+
+  double Elapsed()
+  {
+    fStop = clock::now();
+    return duration_cast<unit>(fStop - fStart).count();
+  }
+
+private:
+  high_resolution_clock::time_point fStart, fStop;
+};
+
+#if !defined(__CUDA_ARCH__) \
+ && (defined(__x86_64__) || defined(_M_X64) \
+    || defined(__i386__) || defined(_M_IX86))
+
+#define VECCORE_TIMER_CYCLES
+
+#ifdef _MSC_VER
+#  include <intrin.h>
+#else
+#  include <x86intrin.h>
+#endif
+
+class cycles {
+};
+
+template <>
+class Timer<cycles> {
+public:
+  Timer() { Start(); }
+
+  void Reset() { Start(); }
+
+  void Start() { fStart = GetCycleCount(); }
+
+  double Elapsed() { return static_cast<double>(GetCycleCount() - fStart); }
+
+private:
+  unsigned long long int fStart;
+
+  inline unsigned long long int GetCycleCount()
+  {
+     return __rdtsc();
+  }
+};
+#endif
+
+#endif
--- test/Test_IncomingOutgoing_v.cpp
+++ test/Test_IncomingOutgoing_v.cpp
@@ -7,8 +7,19 @@
 #include "VisualizationHelper.h"
 #include "base/Vector3D.h"
 #include "colors.h"
+#include "timer.h"
 #include <Imaging.h>
 #include <iostream>
+static constexpr size_t kNruns = 1;//20;
+static constexpr size_t kN = 1;//10000; //(1024 * 1024);
+
+#ifdef VECCORE_TIMER_CYCLES
+using time_unit = cycles;
+static const char *time_unit_name = "cycles";
+#else
+using time_unit = nanoseconds;
+static const char *time_unit_name = "ns";
+#endif
 
 using Tomography::VisualizationHelper;
 using MyVec_t = Tracking::Vector3D<double>;
@@ -44,29 +55,31 @@ vecgeomVec_t POCA(vecgeomVec_t p, vecgeomVec_t u, vecgeomVec_t q, vecgeomVec_t v
   Real_v c = (-uDotv * uDotv + vMag2 * uMag2);
   Real_v d = (-vMag2 * b) / (uDotv * c);
   s = a + d;
-  std::cout << "A : " << a << std::endl;
-  std::cout << "D : " << d << std::endl;
+  /*std::cout << "A : " << a << std::endl;
+  std::cout << "D : " << d << std::endl;*/
   Real_v numer = (uDotv * (pDotu - qDotu) - uMag2 * (pDotv - qDotv));
   Real_v deno = (uDotv * uDotv - uMag2 * vMag2);
   t = numer / deno;
   vecgeomVec_t p1 = p + u * s;
 
-  std::cout << "--------------------------------------" << std::endl;
+  /*std::cout << "--------------------------------------" << std::endl;
   std::cout << "P : " << p << std::endl;
   std::cout << "U : " << u << std::endl;
   std::cout << "S : " << s << std::endl;
-
-  std::cout << "--------------------------------------" << std::endl;
+  std::cout << "--------------------------------------" << std::endl;*/
 
   vecgeomVec_t q1 = q + v * t;
 
-  std::cout << "P1 : " << p1 << std::endl;
-  std::cout << "Q1 : " << q1 << std::endl;
+  /*std::cout << "P1 : " << p1 << std::endl;
+  std::cout << "Q1 : " << q1 << std::endl;*/
   return (p1 + q1) / 2.;
 }
 //------------------------------------------------------
 
 int main(int argc, char *argv[]) {
+  Timer<time_unit> timer;
+  unsigned long long t[kNruns], mean = 0;
+
   Tracking::ImageReconstruction img;
   // TApplication *fApp = new TApplication("Test", NULL, NULL);
   TFile *fp = new TFile(argv[1], "r");
@@ -89,6 +102,24 @@ int main(int argc, char *argv[]) {
   std::vector<Vector3D> pocaVec;
 
   unsigned int counter = 0;
+
+// For Incoming Track
+#ifdef VECGEOM_FLOAT_PRECISION
+  float *x = new float[tr->GetEntries()];
+  float *y = new float[tr->GetEntries()];
+  float *z = new float[tr->GetEntries()];
+  float *dirx = new float[tr->GetEntries()];
+  float *diry = new float[tr->GetEntries()];
+  float *dirz = new float[tr->GetEntries()];
+
+  // For Outgoing Track
+  float *xO = new float[tr->GetEntries()];
+  float *yO = new float[tr->GetEntries()];
+  float *zO = new float[tr->GetEntries()];
+  float *dirxO = new float[tr->GetEntries()];
+  float *diryO = new float[tr->GetEntries()];
+  float *dirzO = new float[tr->GetEntries()];
+#else
   // For Incoming Track
   double *x = new double[tr->GetEntries()];
   double *y = new double[tr->GetEntries()];
@@ -104,6 +135,7 @@ int main(int argc, char *argv[]) {
   double *dirxO = new double[tr->GetEntries()];
   double *diryO = new double[tr->GetEntries()];
   double *dirzO = new double[tr->GetEntries()];
+#endif
 
   for (unsigned int j = 0; j < tr->GetEntries(); j++) {
     // std::cout << RED << "Angle : " << angle << RESET << std::endl;
@@ -136,20 +168,37 @@ int main(int argc, char *argv[]) {
 
   std::cout << BLUE << "*************** Printing PoCA ***********************" << RESET << std::endl;
   // for (unsigned int j = 0; j < tr->GetEntries(); j++) {
-  for (unsigned int j = 0; j < counter; j++) {
-    MyVec_t p(x[j], y[j], z[j]);
-    MyVec_t u(dirx[j], diry[j], dirz[j]);
-    MyVec_t q(xO[j], yO[j], zO[j]);
-    MyVec_t v(dirxO[j], diryO[j], dirzO[j]);
-    MyVec_t p1, q1;
-    MyVec_t poca = img.POCA(p, u, q, v, p1, q1);
-    std::cout << MAGENTA;
-    poca.Print();
-    std::cout << RESET;
-    std::cout << BLUE;
-    pocaVec[j].Print();
-    std::cout << RESET;
+
+  for (size_t n = 0; n < kNruns; n++) {
+    timer.Start();
+    for (size_t i = 0; i < kN; i++) {
+
+      for (unsigned int j = 0; j < counter; j++) {
+        MyVec_t p(x[j], y[j], z[j]);
+        MyVec_t u(dirx[j], diry[j], dirz[j]);
+        MyVec_t q(xO[j], yO[j], zO[j]);
+        MyVec_t v(dirxO[j], diryO[j], dirzO[j]);
+        MyVec_t p1, q1;
+        MyVec_t poca = img.POCA(p, u, q, v, p1, q1);
+        /*std::cout << MAGENTA;
+        poca.Print();
+        std::cout << RESET;
+        std::cout << BLUE;
+        pocaVec[j].Print();
+        std::cout << RESET;*/
+      }
+    }
+    t[n] = timer.Elapsed();
+    std::cout << BLUE << "Time elapsed in iteration number : " << n << " : " << t[n] << RESET << std::endl;
   }
+
+  for (size_t n = 0; n < kNruns; n++)
+    mean += t[n];
+
+  mean = mean / (kN * kNruns);
+  // std::cout << "Time elapsed in vector processing : " << timer.Elapsed() << std::endl;
+  std::cout << "Average Time elapsed in Scalar processing : " << mean << std::endl;
+
   std::cout << RED << "Total Number of Muon Tracks : " << counter << std::endl;
   double color = 10;
 
@@ -159,32 +208,46 @@ int main(int argc, char *argv[]) {
     vecgeomVec_t dirI = (Pi2 - Pi1).Unit();
     vecgeomVec_t Po1(5., 0., 0.), Po2(0., 5., 0.);
     vecgeomVec_t dirO = (Po2 - Po1).Unit();
-    std::cout << "--------------------------------------" << std::endl;
+    // std::cout << "--------------------------------------" << std::endl;
     vecgeomVec_t poca = POCA(Pi1, dirI, Po1, dirO);
-    std::cout << "POCA Point : " << poca << std::endl;
+    // std::cout << "POCA Point : " << poca << std::endl;
 
-    std::cout << BLUE << "############### Going to do Vectorized PoCA #######################" << RESET << std::endl;
+    // std::cout << BLUE << "############### Going to do Vectorized PoCA #######################" << RESET << std::endl;
     unsigned int size = counter;
     unsigned int offset = size - size % vecCore::VectorSize<Real_v>();
     std::cout << "Offset : " << offset << std::endl;
     unsigned int vec_loop_counter = 0;
-    for (unsigned int i = 0; i < offset; i += vecCore::VectorSize<Real_v>()) {
-      // std::cout << RED << "Scalar : (" << x[i] <<","<<x[i+1]<<","<<x[i+2]<<","<<x[i+3] <<")" << RESET << std::endl ;
-      vec_loop_counter++;
-      vecgeom::Vector3D<Real_v> p(vecCore::FromPtr<Real_v>(x + i), vecCore::FromPtr<Real_v>(y + i),
-                                  vecCore::FromPtr<Real_v>(z + i));
-      // std::cout << p << std::endl;
-      vecgeom::Vector3D<Real_v> u(vecCore::FromPtr<Real_v>(dirx + i), vecCore::FromPtr<Real_v>(diry + i),
-                                  vecCore::FromPtr<Real_v>(dirz + i));
+    for (size_t n = 0; n < kNruns; n++) {
+      timer.Start();
+      for (size_t i = 0; i < kN; i++) {
+        for (unsigned int i = 0; i < offset; i += vecCore::VectorSize<Real_v>()) {
+          // std::cout << RED << "Scalar : (" << x[i] <<","<<x[i+1]<<","<<x[i+2]<<","<<x[i+3] <<")" << RESET <<
+          // std::endl ;
+          vec_loop_counter++;
+          vecgeom::Vector3D<Real_v> p(vecCore::FromPtr<Real_v>(x + i), vecCore::FromPtr<Real_v>(y + i),
+                                      vecCore::FromPtr<Real_v>(z + i));
+          // std::cout << p << std::endl;
+          vecgeom::Vector3D<Real_v> u(vecCore::FromPtr<Real_v>(dirx + i), vecCore::FromPtr<Real_v>(diry + i),
+                                      vecCore::FromPtr<Real_v>(dirz + i));
 
-      vecgeom::Vector3D<Real_v> q(vecCore::FromPtr<Real_v>(xO + i), vecCore::FromPtr<Real_v>(yO + i),
-                                  vecCore::FromPtr<Real_v>(zO + i));
-      vecgeom::Vector3D<Real_v> v(vecCore::FromPtr<Real_v>(dirxO + i), vecCore::FromPtr<Real_v>(diryO + i),
-                                  vecCore::FromPtr<Real_v>(dirzO + i));
-      poca = POCA(p, u, q, v);
-      std::cout << BLUE << "POCA : " << poca << RESET << std::endl;
+          vecgeom::Vector3D<Real_v> q(vecCore::FromPtr<Real_v>(xO + i), vecCore::FromPtr<Real_v>(yO + i),
+                                      vecCore::FromPtr<Real_v>(zO + i));
+          vecgeom::Vector3D<Real_v> v(vecCore::FromPtr<Real_v>(dirxO + i), vecCore::FromPtr<Real_v>(diryO + i),
+                                      vecCore::FromPtr<Real_v>(dirzO + i));
+          poca = POCA(p, u, q, v);
+           std::cout << BLUE << "POCA : " << poca << RESET << std::endl;
+        }
+      }
+      t[n] = timer.Elapsed();
+      std::cout << BLUE << "Time elapsed in iteration number : " << n << " : " << t[n] << RESET << std::endl;
     }
 
+    for (size_t n = 0; n < kNruns; n++)
+      mean += t[n];
+
+    mean = mean / (kN * kNruns);
+    // std::cout << "Time elapsed in vector processing : " << timer.Elapsed() << std::endl;
+    std::cout << "Average Time elapsed in vector processing : " << mean << std::endl;
     std::cout << MAGENTA << "Size : " << size << " : Vectorized Loop Counter : " << vec_loop_counter << RESET
               << std::endl;
   }
